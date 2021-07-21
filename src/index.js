fac829b5309929047a29db774d1579bfed3e5919
tdp99 <tdp99.business@gmail.com>
2021-07-21 23:15:57 +0700

    try to push img p1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
+import img from './images/icon/point-icon.png';
+
+function createImgElement() {
+	const imgElement = document.createElement('img');
+	imgElement.src = img;
+	imgElement.alt = 'point-icon';
+	return imgElement;
+}
+
+document.getElementById('hero').appendChild(createImgElement());
+
 const testObj = {
 	apples: 2,
 	pineapples: 1,
 	stones: 4,
 };
 
-const anotherObj = {
-	...testObj,
-	branches: 1,
-	bees: 100,
-};
-
 console.log(testObj);
-console.log(anotherObj);
